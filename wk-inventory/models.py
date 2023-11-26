b8cc444c78da3d7ae3230bf5ea8988eb89198bba
thanawinboon <pokepat27@gmail.com>
2023-11-27 03:35:32 +0700

    completed all functionality

--- wk-inventory/models.py
+++ wk-inventory/models.py
@@ -1,24 +1,21 @@
 import os
 from dotenv import load_dotenv
-from typing import Optional
+from typing import Optional, List
 from sqlmodel import Relationship, SQLModel, Field, Session, create_engine, select
 
 class Thing(SQLModel, table=True):
     # this is item_id
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    price: float
     stock: int = Field(default=0)
     
-    # item_checks: List["ItemCheck"] = Relationship(back_populates="item")
+    item_checks: List["ItemCheck"] = Relationship(back_populates="item")
 
 class ItemCheck(SQLModel, table=True):
-    # check_id
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True) # check_id
     main_id: int
     item_id: int = Field(foreign_key="thing.id")
     quantity: int
-    valid: bool = True
+    valid: bool = Field(default=True)
 
     # Relationship (allows for something like: item_check.item.name)
     item: Thing = Relationship(back_populates="item_checks")
@@ -30,20 +27,28 @@ DB_USER = os.environ.get('DB_USER', 'postgres')
 DB_PASSWORD = os.environ.get('DB_PASSWORD', 'password')
 DB_HOSTNAME = os.environ.get('DB_HOSTNAME', 'localhost')
 DB_PORT = os.environ.get('DB_PORT', '5432')
-DB_NAME = os.environ.get('DB_NAME', 'postgres')
+DB_NAME = os.environ.get('DB_NAME', 'inventory')
 
 db_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(db_url)
-SQLModel.metadata.create_all(engine)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 
-def create_items(name: str, price: float, stock: int):
+def set_item_stock(item_id: int, stock: int):
     with Session(engine) as session:
-        item = Thing(name=name, price=price, stock=stock)
-        session.add(item)
-        session.commit()
-        session.refresh(item)
-        return item
+        statement = select(Thing).where(Thing.id == item_id)
+        try:
+            item = session.exec(statement).one()
+            item.stock = stock
+        except Exception:
+            item = Thing(id=item_id, stock=stock)
+        finally:
+            session.add(item)
+            session.commit()
+            session.refresh(item)
+
 
 def create_item_check(main_id: int, item_id: int, quantity: int):
     with Session(engine) as session:
@@ -59,6 +64,26 @@ def create_item_check(main_id: int, item_id: int, quantity: int):
         # create item_check
         item_check = ItemCheck(main_id=main_id, item_id=item_id, quantity=quantity)
         
+        session.add(item)
+        session.add(item_check)
+        session.commit()
+        session.refresh(item_check)
+        return item_check
+
+def return_item(main_id: int):
+    with Session(engine) as session:
+        # get item_check with main_id
+        statement = select(ItemCheck).where(ItemCheck.main_id == main_id).where(ItemCheck.valid == True)
+        item_check = session.exec(statement).one()
+        
+        # invalidate item_check
+        item_check.valid = False
+
+        # return item to stock
+        statement = select(Thing).where(Thing.id == item_check.item_id)
+        item = session.exec(statement).one()
+        item.stock += item_check.quantity
+
         session.add(item)
         session.add(item_check)
         session.commit()
--- wk-inventory/tasks.py
+++ wk-inventory/tasks.py
@@ -2,29 +2,31 @@ import os
 
 from celery import Celery
 from dotenv import load_dotenv
-from .models import create_item_check, create_items
-from .utils import rollback_payment
+from .models import create_db_and_tables, create_item_check, return_item, set_item_stock
 
 def create_celery_app():
     load_dotenv()
     internal_app = Celery("inventory",
                           broker=f"redis://"
                                  f"{os.environ.get('REDIS_HOSTNAME', 'localhost')}"
-                                 f":{os.environ.get('REDIS_PORT', '6381')}"
+                                 f":{os.environ.get('REDIS_PORT', '6379')}"
                                  f"/", # TODO: change channel
                           backend=f"redis://"
                                   f"{os.environ.get('REDIS_HOSTNAME', 'localhost')}"
-                                  f":{os.environ.get('REDIS_PORT', '6381')}"
+                                  f":{os.environ.get('REDIS_PORT', '6379')}"
                                   f"/", # TODO: change channel
                           broker_connection_retry_on_startup=True)
     return internal_app
 
-
-
 app = create_celery_app()
+create_db_and_tables()
 
 @app.task
-def check_inventory(main_id: int, item_id: int, quantity: int):
+def check_inventory(**kwargs):
+    main_id = kwargs.get('main_id', None)
+    item_id = kwargs.get('item_id', None)
+    quantity = kwargs.get('quantity', None)
+
     # when items are checked out, we need to update the stock
     try:
         create_item_check(main_id=main_id, item_id=item_id, quantity=quantity)
@@ -34,17 +36,22 @@ def check_inventory(main_id: int, item_id: int, quantity: int):
 
 
 @app.task
-def rollback(main_id: int):
+def rollback(**kwargs):
+    main_id = kwargs.get('main_id', None)
     try:
-        rollback_payment(main_id=main_id)
+        return_item(main_id=main_id)
     except Exception as e:
         print(e)
         return False
 
+
 @app.task
-def add_thing(name:str, price:int, stock:int):
+def create_item(**kwargs):
+    item_id = kwargs.get('item_id', None)
+    stock = kwargs.get('stock', None)
+
     try:
-        create_items(name=name, price=price, stock=stock)
+        set_item_stock(item_id=item_id, stock=stock)
     except Exception as e:
         print(e)
         return False
